ab63542f1bcb7b9a18480126bbb216469e7880c8
jcarpio <jose.carpio@gmail.com>
2025-03-23 17:37:57 +0100

    Update credits-plan.ts

--- src/config/credits-plan.ts
+++ src/config/credits-plan.ts
@@ -1,17 +1,17 @@
 export const pricingData = [
   {
     id: 1,
-    price: 0,3,
+    price: '0.5',
     quantity: 1000,
   },
   {
     id: 10,
-    price: 0,99,
+    price: 1,
     quantity: 10000,
   },
   {
     id: 3,
-    price: 0,99,
+    price: 50,
     quantity: 100000,
   }
 ];
