fd852495020a5723560fa9d38fb320c2a9d6d537
jcarpio <jose.carpio@gmail.com>
2025-03-23 09:30:48 +0100

    Update page.tsx

--- src/app/[locale]/(marketing)/page.tsx
+++ src/app/[locale]/(marketing)/page.tsx
@@ -16,7 +16,7 @@ export default function IndexPage({ params: { locale } }: { params: { locale: st
     <>
       <HeroLanding />
       <PreviewLanding />
-      <Powered />
+       {/* <Powered /> */}
       <StepsSection />
       <BentoGrid />
       <InfoLanding/>
