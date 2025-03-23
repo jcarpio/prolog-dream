3921f231b85a89864a9f3f67ddca93f6022a17eb
jcarpio <jose.carpio@gmail.com>
2025-03-23 09:59:59 +0100

    Update marketing.ts

--- src/config/marketing.ts
+++ src/config/marketing.ts
@@ -2,17 +2,5 @@ import { MarketingConfig } from "@/types";
 
 export const marketingConfig: MarketingConfig = {
   mainNav: [
-    {
-      title: "blog",
-      href: "/blog",
-    },
-    {
-      title: "generator-styles",
-      href: "/generator-styles",
-    },
-    {
-      title: "pricing",
-      href: "/pricing",
-    },
   ],
 }
